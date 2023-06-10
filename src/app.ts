57c977485a58eca2506f37928a0bff1c443a8684
JacobHicks <jacobphicks@gmail.com>
2023-06-10 17:08:00 -0400

    ofoiwejf

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,24 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response } from "express";
 import { spawn } from "child_process";
-import cors from 'cors';
+import cors from "cors";
 
 // Spawn llm process
-const llmProcess = spawn('python ~/WizardLM-30B-Uncensored/WizardLM/src/inference_wizardlm.py --base_model=~/WizardLM-30B-Uncensored', {shell: true})
-llmProcess.stdout.setEncoding('utf-8');
-llmProcess.stdin.setDefaultEncoding('utf-8');
+const llmProcess = spawn("python ~/WizardLM-30B-Uncensored/WizardLM/src/inference_wizardlm.py --base_model=~/WizardLM-30B-Uncensored", { shell: true });
+llmProcess.stdout.setEncoding("utf8");
+llmProcess.stdin.setDefaultEncoding("utf8");
 
 const rest: Express = express();
 rest.use(cors());
 rest.use(express.json());
 
-rest.post('/llm', async (req: Request, res: Response) => {
+rest.post("/llm", async (req: Request, res: Response) => {
   console.log(req.body);
   llmProcess.stdin.cork();
-  llmProcess.stdin.write(req.body.prompt + '\r\n');
+  llmProcess.stdin.write(req.body.prompt + "\r\n");
   llmProcess.stdin.uncork();
-  console.log('Weewoo')
 
   const llmOutput = await new Promise((resolve, reject) => {
-    llmProcess.stdout.on('data', data => {
+    llmProcess.stdout.on("data", data => {
       console.info(`Model output: ${data}`);
       try {
         const parsedOutput: { id: number, instruction: string, wizardlm: string } = JSON.parse(data);
@@ -29,9 +28,14 @@ rest.post('/llm', async (req: Request, res: Response) => {
         reject(e);
       }
     });
-  });
 
-  res.json({llmOutput});
-})
+    llmProcess.stderr.on("data", data => {
+      console.error(`Model error: ${data}`);
+      reject(data);
+    });
+
+    res.json({ llmOutput });
+  });
+});
 
 rest.listen(8000);
