c2a8c84680c02248a21c5c0d11a2acffedfb3921
JacobHicks <jacobphicks@gmail.com>
2023-06-10 17:26:42 -0400

    ofoiwejf

--- src/app.ts
+++ src/app.ts
@@ -7,35 +7,48 @@ const llmProcess = spawn("python ~/WizardLM-30B-Uncensored/WizardLM/src/inferenc
 llmProcess.stdout.setEncoding("utf8");
 llmProcess.stdin.setDefaultEncoding("utf8");
 
+let modelLoaded = false;
+console.log("Waiting for model to load...");
+llmProcess.stdout.on("data", data => {
+  if (data.includes('#')) {
+    console.log("Model loaded!");
+    modelLoaded = true;
+  }
+});
+
 const rest: Express = express();
 rest.use(cors());
 rest.use(express.json());
 
 rest.post("/llm", async (req: Request, res: Response) => {
-  console.log(req.body);
+  if (modelLoaded) {
+    console.log(req.body);
 
-  const llmOutput = await new Promise((resolve) => {
-    console.log("Waiting for model output...");
-    llmProcess.stdout.on("data", data => {
-      console.info(`Model output: ${data}`);
-      try {
-        const parsedOutput: { id: number, instruction: string, wizardlm: string } = JSON.parse(data);
-        resolve(parsedOutput.wizardlm);
-      } catch (e) {
-        console.error(e);
-      }
+    const llmOutput = await new Promise((resolve) => {
+      console.log("Waiting for model output...");
+      llmProcess.stdout.on("data", data => {
+        console.info(`Model output: ${data}`);
+        try {
+          const parsedOutput: { id: number, instruction: string, wizardlm: string } = JSON.parse(data);
+          resolve(parsedOutput.wizardlm);
+        } catch (e) {
+          console.error(e);
+        }
+      });
+
+      llmProcess.stderr.on("data", data => {
+        console.error(`Model error: ${data}`);
+      });
+
+      llmProcess.stdin.cork();
+      llmProcess.stdin.write(req.body.prompt + "\r\n");
+      llmProcess.stdin.uncork();
     });
 
-    llmProcess.stderr.on("data", data => {
-      console.error(`Model error: ${data}`);
-    });
-
-    llmProcess.stdin.cork();
-    llmProcess.stdin.write(req.body.prompt + "\r\n");
-    llmProcess.stdin.uncork();
-  });
-
-  res.json({ llmOutput });
+    res.json({ llmOutput });
+  } else {
+    res.json({ error: "Model not loaded yet." });
+  }
 });
 
 rest.listen(8000);
